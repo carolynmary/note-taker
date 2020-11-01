2cbdae17627538886f8789c83d18aa5cabc6a281
CaroS <carolyn.mary@hotmail.com>
2020-10-31 21:15:34 -0400

    pushing for BCS assistance

---
+++ routes/apiRoutes.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const path = require("path");
+const router = require("express").Router();
+const util = require("util");
+// const db = path.join(__dirname, "../db/db.json");
+// const db = require("../db/db.json");
+// const db = require(JSON.parse(path.join(__dirname, "../db/db.json")));
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// GET 
+// ---------------------------------------------------------------------------
+router.get("/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, notes) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json(err)
+    }
+    // console.log(JSON.parse(notes))
+    return res.json(JSON.parse(notes));
+  })
+});
+
+// POST 
+// ---------------------------------------------------------------------------
+router.post("/notes", async (req, res) => {
+  let newNote = req.body;
+  newNote.id = Date.now();
+  let notes = await readFileAsync(path.join(__dirname, "../db/db.json"));
+  let parsedNotes = JSON.parse(notes);
+  fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify([...parsedNotes, newNote], null, 2), (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json(err)
+    };
+    res.json(true);
+  });
+});
+
+// DELETE - filtered function not working
+// ---------------------------------------------------------------------------
+router.delete("/notes/:id", async (req, res) => {
+  let queryNoteId = req.params.id
+  console.log("query note to delete: " + queryNoteId);
+  let notes = await readFileAsync(path.join(__dirname, "../db/db.json"));
+  let parsedNotes = JSON.parse(notes);
+  console.log(parsedNotes); // returns the correct array
+  let filteredNotes = parsedNotes.filter((note) => note.id !== queryNoteId);
+  console.log(filteredNotes);  // returns the same as parsedNotes (2 lines above)
+  fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(filteredNotes, null, 2), (err) => {
+    console.log("inside writeFilteredNotes")
+    if (err) {
+      console.log(err);
+      return res.status(500).json(err)
+    };
+    res.json(true);
+  })
+});
+
+module.exports = router;
+
+// function compareId (note) {
+//   return note.id !== queryNoteId
+// } 
+// let filteredNotes = parsedNotes.filter(compareId);
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
