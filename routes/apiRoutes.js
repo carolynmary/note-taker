faa932c08133f22a5f2867b5efe3acdf219bc98a
CaroS <carolyn.mary@hotmail.com>
2020-11-02 19:05:21 -0500

    get-post working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -38,29 +38,129 @@ router.post("/notes", async (req, res) => {
   });
 });
 
-// DELETE - filtered function not working
+// DELETE - filter function not working
 // ---------------------------------------------------------------------------
-router.delete("/notes/:id", async (req, res) => {
-  let queryNoteId = req.params.id
-  console.log("query note to delete: " + queryNoteId);
-  let notes = await readFileAsync(path.join(__dirname, "../db/db.json"));
+router.delete("/notes/:id", (req, res) => {
+  let noteToDeleteID = req.params.id
+  console.log("note to delete id: " + noteToDeleteID);
+  let notes = fs.readFile(path.join(__dirname, "../db/db.json"));
   let parsedNotes = JSON.parse(notes);
-  console.log(parsedNotes); // returns the correct array
-  let filteredNotes = await parsedNotes.filter((note) => note.id !== queryNoteId);
-  console.log(filteredNotes);  // returns the same as parsedNotes (2 lines above)
-  fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(filteredNotes, null, 2), (err) => {
-    console.log("inside writeFilteredNotes")
-    if (err) {
-      console.log(err);
-      return res.status(500).json(err)
-    };
-    res.json(true);
-  })
+  // let notes = await JSON.parse(readFileAsync(path.join(__dirname, "../db/db.json"))); // try later
+  console.log("PARSED: ", parsedNotes);
+
+  return parsedNotes
+    .then((notes) => notes.filter((note) => note.id !== noteToDeleteID))
+    .then((filteredNotes) => write(filteredNotes))
+  // console.log("FILTERED: ", filteredNotes);
+
+  function write(notes) {
+    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes, null, 2), (err) => {
+      console.log("inside writeFilteredNotes")
+      if (err) {
+        console.log(err);
+        return res.status(500).json(err)
+      };
+      res.json(true);
+    })
+  }
 });
 
 module.exports = router;
 
+// router.delete("/notes/:id", async (req, res) => {
+//   let queryNoteId = req.params.id
+//   console.log("query note to delete: " + queryNoteId);
+//   let notes = await readFileAsync(path.join(__dirname, "../db/db.json"));
+//   let parsedNotes = JSON.parse(notes);
+//   // let notes = await readFileAsync(JSON.parse(path.join(__dirname, "../db/db.json")));
+//   console.log(parsedNotes);
+
+//   let filteredNotes = parsedNotes.filter((noteArrayItem) => filteredNotes(noteArrayItem.id !== queryNoteId));
+
+//   console.log("FILTERED: ", filteredNotes);
+
+//   fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(filteredNotes, null, 2), (err) => {
+//     console.log("inside writeFilteredNotes")
+//     if (err) {
+//       console.log(err);
+//       return res.status(500).json(err)
+//     };
+//     res.json(true);
+//   })
+// });
+
+
+
+// let filteredNotes = parsedNotes.filter((noteArrayItem) => {
+//   console.log("insideFilter noteArrayItem.id: " + noteArrayItem.id);
+//   console.log("insideFilter queryNoteId: " + queryNoteId);
+//   if (noteArrayItem.id === queryNoteId) { 
+//     return false 
+//   }
+//   else { 
+//     return true 
+//   }
+// });
+
+
+
+// let filteredNotes = parsedNotes.filter((noteArrayItem) => filteredNotes(noteArrayItem.id !== queryNoteId));
+// let filteredNotes = parsedNotes.filter((noteArrayItem) => noteArrayItem.id !== queryNoteId);
+
+
+
+
+// AWAIT FILTER ----------------------------------------------------------------
+// let filteredNotes = await parsedNotes.map((noteArrayItem) => {
+//   if (noteArrayItem.id !== queryNoteId) {
+//     return noteArrayItem;
+//   }
+// });
+
+
+// FUNCTION THEN FILTER ----------------------------------------------------------------
 // function compareId (note) {
 //   return note.id !== queryNoteId
 // } 
 // let filteredNotes = parsedNotes.filter(compareId);
+
+
+
+// FOR EACH ----------------------------------------------------------------
+// let filteredNotes = [];
+  // parsedNotes.forEach(function (noteArrayItem) {
+  //   if ((parsedNotes.id).includes(queryNoteId)) {
+  //     console.log(id)
+  //   }
+  //   else {
+  //     filteredNotes.push(noteArrayItem); // add to a NEW array
+  //   }
+  // });
+
+
+// FOR LOOP ----------------------------------------------------------------
+// let filteredNotes = [];
+  // function compareId() {
+  //   for (let i = 0; i < parsedNotes.length; i++) {
+  //     console.log(parsedNotes[i].id)
+  //     if (parsedNotes[i].id === queryNoteId) {
+  //       return
+  //     }
+  //     else {
+  //       filteredNotes.push(parsedNotes[i])
+  //     }
+  //   }
+  // };
+  // compareId()
+
+
+// FOR IN ----------------------------------------------------------------
+// let filteredNotes = [];
+  // for (const id in parsedNotes) {
+  //   if (id === queryNoteId) {
+  //     console.log(id)
+  //   }
+  //   else {
+  //     filteredNotes.push(parsedNotes)
+  //   }
+  // }
