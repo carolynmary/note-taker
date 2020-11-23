26cc9fe32deeceba487e0c4610437cb2943e17cb
CaroS <carolyn.mary@hotmail.com>
2020-11-22 23:49:19 -0500

    read/write now async

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,46 @@
 const fs = require('fs');
 const path = require("path");
 const router = require("express").Router();
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 let allNotes;
 
+// GET 
+router.get("/notes", (req, res) => {
+  res.json(allNotes)
+});
+
+// POST 
+router.post("/notes", (req, res) => {
+  let newNote = req.body;
+  newNote.id = Date.now();
+  allNotes.push(newNote);
+  let stringifiedNotes = JSON.stringify(allNotes, null, 2)
+  write(stringifiedNotes);
+  res.json(allNotes);  // - new
+  read();
+});
+
+// DELETE
+router.delete("/notes/:id", (req, res) => {
+  noteToDeleteID = JSON.parse(req.params.id);
+  let filteredNotes = allNotes.filter(note => (note.id !== noteToDeleteID));
+  let stringifiedNotes = JSON.stringify(filteredNotes, null, 2)
+  write(stringifiedNotes);
+  res.json(allNotes);
+  read()
+});
+
+// ===================================================================================================================
+// FUNCTIONS
+// ===================================================================================================================
+
 // READ 
-// ---------------------------------------------------------------------------
-function read() {
-  fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function (err, data) {
+async function read() {
+  await readFileAsync(path.join(__dirname, "../db/db.json"), "utf8", function (err, data) {
     if (err) {
       console.log(err);
       return res.status(500).json(err)
@@ -18,9 +51,8 @@ function read() {
 read();
 
 // WRITE 
-// ---------------------------------------------------------------------------
-function write(notes) {
-  fs.writeFile(path.join(__dirname, "../db/db.json"), notes, (err) => {
+async function write(notes) {
+  await writeFileAsync(path.join(__dirname, "../db/db.json"), notes, (err) => {
     if (err) {
       console.log(err);
       return res.status(500).json(err)
@@ -28,31 +60,5 @@ function write(notes) {
     res.json(allNotes);
   })
 }
-
-// GET 
-// ---------------------------------------------------------------------------
-router.get("/notes", (req, res) => {
-  res.json(allNotes)
-});
-
-// POST 
-// ---------------------------------------------------------------------------
-router.post("/notes", (req, res) => {
-  let newNote = req.body;
-  newNote.id = Date.now();
-  allNotes.push(newNote);
-  let stringifiedNotes = JSON.stringify(allNotes, null, 2)
-  write(stringifiedNotes);
-});
-
-// DELETE 
-// ---------------------------------------------------------------------------
-router.delete("/notes/:id", (req, res) => {
-  noteToDeleteID = JSON.parse(req.params.id);
-  let filteredNotes = allNotes.filter(note => (note.id !== noteToDeleteID));
-  let stringifiedNotes = JSON.stringify(filteredNotes, null, 2)
-  write(stringifiedNotes);
-  read()
-});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
 const express = require("express");
 const app = express();
 
@@ -6,9 +9,6 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
